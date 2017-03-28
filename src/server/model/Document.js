92afd020032da7a1c8a0eaf49812967a68bbd068
Donald Atkinson <donald.atkinson@fuzzicallogic.com>
2017-03-28 14:36:08 -0600

    Add object properties

--- src/server/model/Document.js
+++ src/server/model/Document.js
@@ -1,12 +1,33 @@
 module.exports = (function defineClass() {
 
     const Route = require('./Route.js');
-    class Page extends Route {
-        constructor() {
+    class Document extends Route {
+        constructor(config = {}) {
             super();
+
+            let {_key, route, code} = config;
+            console.log(config);
+            let properties = Object.create(null);
+            instances.set(this, properties);
+
+            properties.key = _key;
+            properties.subclass = this.className;
+            properties.route = route;
+            properties.code = code;
         }
 
+        get key() {
+            return instances.get(this).key;
+        }
+
+        get route() {
+            return instances.get(this).route;
+        }
+
+        get code() {
+            return instances.get(this).path;
+        }
     }
 
-    return Page;
+    return Document;
 }) ();
